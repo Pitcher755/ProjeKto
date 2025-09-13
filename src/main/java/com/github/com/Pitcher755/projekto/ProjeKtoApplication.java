94a38183435a74ed9b92abd9db3ff05e6045726d
PitcherDev <cantaro755@gmail.com>
2025-09-13 21:10:56 +0200

    Configuración inical de Projekto, listo para subir al main, configuradas dependencias en el pom.xml y las aplications.properties

---
+++ src/main/java/com/github/com/Pitcher755/projekto/ProjeKtoApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.Pitcher755.projekto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjeKtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjeKtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/com/Pitcher755/projekto/ProjeKtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.Pitcher755.projekto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeKtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
